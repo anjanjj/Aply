c0b73cbd21d445fcf13886c18f532a1c490054fe
anjanjj <anjavlacic75@gmail.com>
2022-10-28 14:27:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+In this project we used local web server.
+
+Complete API which is needed for developing is here:
+
+https://github.com/nenadbugaric/interviews-reports-api-mock/
+
+Set up instructions:
+git clone https://github.com/nenadbugaric/interviews-reports-api-mock/
+git checkout auth-client
+npm install
+npm start
+
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Footer.scss";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="person">
+                <img className="person-img"src='https://scontent.fbeg5-1.fna.fbcdn.net/v/t1.18169-9/10268420_235593936631566_1539214007138220024_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=d39ESTteBx4AX-RFsTu&tn=w-vvnLsjHjqLpseE&_nc_ht=scontent.fbeg5-1.fna&oh=00_AfClr91c96IfyJxWIktFXjQZL8MiYga1leI_rJIHRaKNSA&oe=6380BC05' alt=''></img>
+                <p>Dalibor Gavrilović</p>
+                <p>gavra43@gmail.com</p>
+            </div>
+            <div className="person">
+                <img className="person-img"src='https://media-exp1.licdn.com/dms/image/C4E03AQH6ljzgRkns8A/profile-displayphoto-shrink_800_800/0/1600764231741?e=1672272000&v=beta&t=bhbL-fpHYfN8TkEkntetjZhOsgm3K-noYz1liihIgZE' alt=''></img>
+                <p>Teodora Lečovska</p>
+                <p>lecovska@gmail.com</p>
+            </div>
+            <div className="person">
+                <img className="person-img"src='https://media-exp1.licdn.com/dms/image/C4E03AQFlWTnIEyn0UQ/profile-displayphoto-shrink_800_800/0/1611414371400?e=1672272000&v=beta&t=StXJUrs4SSZwQ80levZmpXkTQP4etybXq3yt8kNTYpw' alt=''></img>
+                <p>Martin Potkonjak</p>
+                <p>mar.potkonjak@gmail.com</p>
+            </div>
+            <div className="person">
+                <img className="person-img"src='https://media-exp1.licdn.com/dms/image/C4D03AQG2wHKfpa7LkQ/profile-displayphoto-shrink_800_800/0/1658341671547?e=1672272000&v=beta&t=v_mYgFSbjKMO-PTgko6-rydhHI9qQjagH6Kn-J0gsyA' alt=''></img>
+                <p>Anja Vlačić</p>
+                <p>anjavlacic75@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
